9f016d53849d50f4f8aac6d77e0499b8eb92fdc7
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-09-24 08:26:53 -0700

    0.1 Documentation - added studio name to self and party stats embeds

--- Data/Buttons/partystats.js
+++ Data/Buttons/partystats.js
@@ -11,7 +11,8 @@ module.exports.execute = (interaction, args) => {
 		.setTitle("Party Stats")
 		.setDescription(`${adventure.name} - Depth: ${adventure.depth}`)
 		.addField(`${adventure.lives} Lives Remain`, "When a player runs out of HP, a life will be lost and they'll be returned to max HP. When all lives are lost, the adventure will end.")
-		.addField(`${adventure.gold} Gold`, "Gold is exchanged for goods and services within adventures. Gold *will be lost when an adventure ends*.");
+		.addField(`${adventure.gold} Gold`, "Gold is exchanged for goods and services within adventures. Gold *will be lost when an adventure ends*.")
+        .setFooter("Imaginary Horizons Productions", "https://cdn.discordapp.com/icons/353575133157392385/c78041f52e8d6af98fb16b8eb55b849a.png");
 	interaction.reply({ embeds: [embed], ephemeral: true })
 		.catch(console.error);
 }
--- Data/Buttons/self.js
+++ Data/Buttons/self.js
@@ -10,7 +10,8 @@ module.exports.execute = (interaction, args) => {
 	let delver = adventure.delvers.find(delver => delver.id === interaction.user.id);
 	let embed = new MessageEmbed()
 		.setTitle(delver.name)
-		.setDescription(`HP: ${delver.hp}/${delver.maxHp}\nReads: ${delver.readType}`);
+		.setDescription(`HP: ${delver.hp}/${delver.maxHp}\nReads: ${delver.readType}`)
+		.setFooter("Imaginary Horizons Productions", "https://cdn.discordapp.com/icons/353575133157392385/c78041f52e8d6af98fb16b8eb55b849a.png");
 	for (let i = 0; i < delver.weapons.length; i++) {
 		let weapon = delver.weapons[i];
 		embed.addField(`Weapon ${i + 1}: ${weapon.name}`, `Uses: ${weapon.uses}/${weapon.maxUses}\nElement: ${weapon.element}\n${weapon.description}`);
