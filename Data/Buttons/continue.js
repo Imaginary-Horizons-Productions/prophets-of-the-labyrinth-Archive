75eb0add10cf78a9cc9f972af53421deded027df
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-11-22 18:48:24 -0800

    0.2 Fix - fixed continue message appearing after next room message

--- Data/Buttons/continue.js
+++ Data/Buttons/continue.js
@@ -6,8 +6,8 @@ module.exports = new Button("continue");
 module.exports.execute = (interaction, args) => {
 	// Generate the next room of an adventure
 	let adventure = getAdventure(interaction.channel.id);
-	interaction.message.edit({ components: [] })
-		.catch(console.error);
-	interaction.reply({ content: `The party moves on.`, ephemeral: true });
-	nextRoom(adventure, interaction.channel);
+	interaction.message.edit({ components: [] });
+	interaction.reply({ content: `The party moves on.`, ephemeral: true }).then(message => {
+		nextRoom(adventure, interaction.channel);
+	});
 }
