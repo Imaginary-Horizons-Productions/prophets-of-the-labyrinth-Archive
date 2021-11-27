0573a8815c891c3e7913711b891dbaf3bb9b122d
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-11-27 09:01:44 -0800

    0.2 Content - made hemomancer available for new players

--- Classes/Player.js
+++ Classes/Player.js
@@ -5,5 +5,5 @@ module.exports = class Player {
 	}
 	scores = {};
 	artifacts = {};
-	archetypes = { "Knight": 1, "Assassin": 1, "Chemist": 1, "Martial Artist": 1 }; //TODO #36 roll starting classes randomly
+	archetypes = { "Knight": 1, "Assassin": 1, "Chemist": 1, "Martial Artist": 1, "Hemomancer": 1 }; //TODO #36 roll starting classes randomly
 }
