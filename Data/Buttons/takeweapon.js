ebcda25b1fb8fcfaa519644c8876cf10e572dbc2
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-11-27 07:35:01 -0800

    0.2 Fix - fixed players being able to obtain more than 4 weapons

--- Data/Buttons/takeweapon.js
+++ Data/Buttons/takeweapon.js
@@ -8,26 +8,31 @@ module.exports = new Button("takeweapon");
 module.exports.execute = (interaction, [weaponName]) => {
 	// Add the given weapon the the delver's weapon list
 	let adventure = getAdventure(interaction.channel.id);
-	adventure.delvers.find(delver => delver.id === interaction.user.id).weapons[weaponName] = getWeaponProperty(weaponName, "maxUses");
-	let remaining = --adventure.room.loot[`weapon-${weaponName}`];
-	interaction.update({
-		components: [...interaction.message.components.map(row => {
-			return new MessageActionRow().addComponents(...row.components.map(component => {
-				if (component.customId.startsWith("takeweapon")) {
-					if (remaining !== 0) {
-						return component.setLabel(`${weaponName} x${remaining} remaining`);
+	let delver = adventure.delvers.find(delver => delver.id === interaction.user.id);
+	if (Object.keys(delver.weapons).length < 4) {
+		delver.weapons[weaponName] = getWeaponProperty(weaponName, "maxUses");
+		let remaining = --adventure.room.loot[`weapon-${weaponName}`];
+		interaction.update({
+			components: [...interaction.message.components.map(row => {
+				return new MessageActionRow().addComponents(...row.components.map(component => {
+					if (component.customId.startsWith("takeweapon")) {
+						if (remaining !== 0) {
+							return component.setLabel(`${weaponName} x${remaining} remaining`);
+						} else {
+							return component.setLabel(`${weaponName} GET`)
+								.setDisabled(true)
+								.setEmoji("✔️");
+						}
 					} else {
-						return component.setLabel(`${weaponName} GET`)
-							.setDisabled(true)
-							.setEmoji("✔️");
+						return component;
 					}
-				} else {
-					return component;
-				}
-			}));
-		})
-		]
-	});
-	interaction.followUp({ content: `${interaction.member.displayName} takes a ${weaponName}.` });
-	setAdventure(adventure);
+				}));
+			})
+			]
+		});
+		interaction.followUp({ content: `${interaction.member.displayName} takes a ${weaponName}.` });
+		setAdventure(adventure);
+	} else {
+		interaction.reply("You can only carry 4 weapons at a time. (Weapon replacement coming soon).");
+	}
 }
