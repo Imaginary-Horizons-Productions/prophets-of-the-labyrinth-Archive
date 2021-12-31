af4f71cb5e52a764fd5df67d945015335bbd0321
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-12-31 12:41:39 -0800

    0.4 Fix - removed extra space from changed room vote reply

--- Data/Buttons/routevote.js
+++ Data/Buttons/routevote.js
@@ -35,7 +35,7 @@ module.exports.execute = (interaction, [direction]) => {
 		let embed = interaction.message.embeds[0];
 		embed.spliceFields(embed.fields.findIndex(field => field.name === "Decide the next room"), 1, { name: "Decide the next room", value: routingText });
 		interaction.message.edit({ embeds: [embed] });
-		interaction.reply(`${interaction.user} ${changeVote ? "changed votes to " : "voted for"} ${direction}.`).then(() => {
+		interaction.reply(`${interaction.user} ${changeVote ? "changed votes to" : "voted for"} ${direction}.`).then(() => {
 			// Decide by unanimous vote
 			if (adventure.roomCandidates[direction].length === adventure.delvers.length) {
 				embed.spliceFields(embed.fields.findIndex(field => field.title === "Decide the next room"), 1);
