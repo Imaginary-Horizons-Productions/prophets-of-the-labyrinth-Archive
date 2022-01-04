5d493c60c85559888d1734887ae1ddd8161cb558
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-01-04 09:58:24 -0800

    0.4 Improvements and Fix - optimizations on commandDictionary - fixed /reset being imported as setup.js

--- Data/Commands/_commandDictionary.js
+++ Data/Commands/_commandDictionary.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const CommandSet = require('../../Classes/CommandSet.js');
 
-// The help command supports 25 command sets to conform with MessageEmbed limit of 25 fields
+// A maximum of 25 command sets are supported by /commands to conform with MessageEmbed limit of 25 fields
 let generalCommands = new CommandSet("Game Commands", "Here are the commands you'll use when playing Dungeon Tamers", false, ["delve.js", "party-stats.js", "delver-stats.js", "give-up.js"]);
 let infoCommands = new CommandSet("Informational Commands", "Use these commands to look up information about Dungeon Tamers!", false, ["about.js", "stats.js", "feedback.js"])
-let configCommands = new CommandSet("Configuration Commands", "These commands change how the bot operates on your server. They require bot management permission (a role above the bot's roles).", true, ["setup.js"])
+let configCommands = new CommandSet("Configuration Commands", "These commands change how the bot operates on your server. They require bot management permission (a role above the bot's roles).", true, ["reset.js"])
 // let moderationCommands = new CommandSet("Moderation Commands", "These commands allow bot managers to counteract bad behavior. They require bot management permission (a role above the bot's roles).", true, [])
 
 exports.commandSets = [
@@ -14,11 +14,7 @@ exports.commandSets = [
 	// moderationCommands
 ];
 
-var commandFileNames = [];
-this.commandSets.forEach(set => {
-	commandFileNames = commandFileNames.concat(set.fileNames);
-})
-const commandFiles = fs.readdirSync('./Data/Commands').filter(file => file.endsWith('.js') && commandFileNames.includes(file));
+const commandFiles = this.commandSets.reduce((allFiles, set) => allFiles.concat(set.fileNames), []);
 exports.commandDictionary = {};
 exports.slashData = [];
 
