9588a77ffae54e74bce7d43251049cfce71c8fae
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-01-19 10:22:47 -0800

    v0.4 Improvement - optimization on commandDictionary

--- Data/Commands/_commandDictionary.js
+++ Data/Commands/_commandDictionary.js
@@ -1,16 +1,10 @@
-const fs = require('fs');
 const CommandSet = require('../../Classes/CommandSet.js');
 
 // A maximum of 25 command sets are supported by /commands to conform with MessageEmbed limit of 25 fields
-let generalCommands = new CommandSet("Game Commands", "Here are the commands you'll use when playing Dungeon Tamers", false, ["delve.js", "party-stats.js", "delver-stats.js", "give-up.js"]);
-let infoCommands = new CommandSet("Informational Commands", "Use these commands to look up information about Dungeon Tamers!", false, ["about.js", "stats.js", "feedback.js"])
-let configCommands = new CommandSet("Configuration Commands", "These commands change how the bot operates on your server. They require bot management permission (a role above the bot's roles).", true, ["reset.js"])
-// let moderationCommands = new CommandSet("Moderation Commands", "These commands allow bot managers to counteract bad behavior. They require bot management permission (a role above the bot's roles).", true, [])
-
 exports.commandSets = [
-	generalCommands,
-	infoCommands,
-	configCommands,
+	new CommandSet("Game Commands", "Here are the commands you'll use when playing Dungeon Tamers", false, ["delve.js", "party-stats.js", "delver-stats.js", "give-up.js"]),
+	new CommandSet("Informational Commands", "Use these commands to look up information about Dungeon Tamers!", false, ["about.js", "stats.js", "feedback.js"]),
+	new CommandSet("Configuration Commands", "These commands change how the bot operates on your server. They require bot management permission (a role above the bot's roles).", true, ["reset.js"]),
 	// moderationCommands
 ];
 
