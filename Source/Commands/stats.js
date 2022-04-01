36779cf5a904e6c10eb073715fcf1d81a8d4ebf7
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-04-01 08:45:12 -0700

    v0.7.0 Feature - /stats shows player availability

--- Source/Commands/stats.js
+++ Source/Commands/stats.js
@@ -7,18 +7,32 @@ const options = [
 module.exports = new Command("stats", "Get the stats for a user (default: yourself)", false, false, options);
 
 // imports from files that depend on /Config
-let getArtifactCounts, getPlayer;
+let
+	// artifactDictionary
+	getArtifactCounts,
+	// playerDAO
+	getPlayer,
+	// helpers
+	isSponsor,
+	// guildDAO
+	getGuild;
 module.exports.injectConfig = function (isProduction) {
 	({ getArtifactCounts } = require('../Artifacts/_artifactDictionary.js').injectConfigArtifacts(isProduction));
 	({ getPlayer } = require('../playerDAO.js').injectConfig(isProduction));
+	({ isSponsor } = require('../../helpers.js').injectConfig(isProduction));
+	({ getGuild, getGuild } = require('../guildDAO.js').injectConfig(isProduction));
 	return this;
 }
 
 module.exports.execute = (interaction) => {
 	// Get the stats on a user
+	let availability = getGuild(interaction.guildId).adventuring.has(interaction.user.id) ? "❌ Out on adventure" : "🟢 Available for adventure";
+	if (isSponsor(interaction.user.id)) {
+		availability = "💎 Premium (available for adventure)";
+	}
 	let player = getPlayer(interaction.user.id, interaction.guild.id);
 	let totalArtifacts = getArtifactCounts();
-	let embed = new MessageEmbed()
+	let embed = new MessageEmbed().setAuthor({ name: availability })
 		.setTitle("Player Stats")
 		.setDescription(`Total Score: ${Object.values(player.scores).reduce((total, current) => total += current)}`)
 		.addField("Artifacts Collected", `${Object.values(player.artifacts).length}/${totalArtifacts} Artifacts (${Math.floor(Object.values(player.artifacts).length / totalArtifacts * 100)}%)`)
