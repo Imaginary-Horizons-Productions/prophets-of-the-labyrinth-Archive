2b4a7a1064e1b5351233100ce662dd342aa12e80
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-04-01 19:56:14 -0700

    v0.7.0 Balance - added more progress to elkemist

--- Source/Enemies/elkemist.js
+++ Source/Enemies/elkemist.js
@@ -38,9 +38,9 @@ function toilEffect(target, user, isCrit, adventure) {
 	// Gain block and medium progress
 	removeModifier(user, { name: "Stagger", stacks: 1 });
 	if (isCrit) {
-		addModifier(user, { name: "Progress", stacks: 20 + generateRandomNumber(adventure, 16, "battle") });
+		addModifier(user, { name: "Progress", stacks: 60 + generateRandomNumber(adventure, 46, "battle") });
 	} else {
-		addModifier(user, { name: "Progress", stacks: 15 + generateRandomNumber(adventure, 11, "battle") });
+		addModifier(user, { name: "Progress", stacks: 45 + generateRandomNumber(adventure, 31, "battle") });
 	}
 	addBlock(user, 200);
 	return "It succeeds at gathering some materials and fortifying its laboratory.";
@@ -52,6 +52,7 @@ function troubleEffect(target, user, isCrit, adventure) {
 	if (isCrit) {
 		damage *= 2;
 	}
+	addModifier(user, { name: "Progress", stacks: 15 + generateRandomNumber(adventure, 16, "battle") });
 	addModifier(target, { name: "Stagger", stacks: 1 });
 	return dealDamage(target, user, damage, false, user.element, adventure).then(damageText => {
 		return `An obstacle to potion progress is identified and mitigated; ${damageText}`;
@@ -70,9 +71,9 @@ function boilEffect(target, user, isCrit, adventure) {
 function bubbleEffect(target, user, isCrit, adventure) {
 	// Remove buffs from all foes and big progress
 	if (isCrit) {
-		addModifier(user, { name: "Progress", stacks: 10 + generateRandomNumber(adventure, 6, "battle") });
+		addModifier(user, { name: "Progress", stacks: 30 + generateRandomNumber(adventure, 16, "battle") });
 	} else {
-		addModifier(user, { name: "Progress", stacks: 5 + generateRandomNumber(adventure, 6, "battle") });
+		addModifier(user, { name: "Progress", stacks: 15 + generateRandomNumber(adventure, 16, "battle") });
 	}
 	for (let modifier in target.modifiers) {
 		if (isBuff(modifier)) {
