a749ca0a992a3473d5379cbfad5053807c1c148b
Vivian Thach <vivianlthach@gmail.com>
2022-05-02 18:09:10 -0700

    combined free gold and gold on fire - commented out other 2 events for now

--- Source/Buttons/getgoldonfire.js
+++ Source/Buttons/getgoldonfire.js
@@ -1,6 +1,7 @@
 const Button = require('../../Classes/Button.js');
 const { getAdventure, completeAdventure, updateRoomHeader, setAdventure } = require('../adventureDAO.js');
 const { dealDamage } = require("../combatantDAO.js");
+const { editButtons } = require('../roomDAO.js');
 
 module.exports = new Button("getgoldonfire");
 
@@ -8,15 +9,17 @@ module.exports.execute = (interaction, args) => {
 	// Gold +50, HP -100
 	let adventure = getAdventure(interaction.channel.id);
 	let delver = adventure.delvers.find(delver => delver.id == interaction.user.id);
-	if (delver) {
+	let goldCount = adventure.room.resources.gold.count;
+  if (delver) {
 		adventure.gainGold(50);
 		dealDamage(delver, null, 100, true, "Untyped", adventure).then(damageText => {
 			updateRoomHeader(adventure, interaction.message);
 			if (adventure.lives < 1) {
 				interaction.reply({ embeds: [completeAdventure(adventure, interaction.channel, { isSuccess: false, description: null })] });
 			} else {
+        interaction.update({ components: editButtons(interaction.message.components, { "getgoldonfire": { preventUse: true, label: `+${goldCount} gold`, emoji: "✔️" } }) })
+				interaction.channel.send(`${interaction.user} reaches in to grab some coin. Oh no! The gold burst into flames ${damageText}`);
 				setAdventure(adventure);
-				interaction.reply(`${interaction.user} reaches into the flames and grabs some coin. ${damageText}`);
 			}
 		});
 	} else {
