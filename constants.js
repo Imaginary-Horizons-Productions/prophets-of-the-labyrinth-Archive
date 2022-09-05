41fb9f4561c8020f9ab5bdcc529ccbda76c2c27c
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-09-05 13:26:05 -0700

    #412: make maxDelverCount dynamic

--- constants.js
+++ constants.js
@@ -2,4 +2,4 @@ module.exports.SAFE_DELIMITER = "→";
 module.exports.MAX_MESSAGE_ACTION_ROWS = 5;
 module.exports.MAX_BUTTONS_PER_ROW = 5;
 module.exports.MAX_SELECT_OPTIONS = 25;
-module.exports.maxDelverCount = 8;
+module.exports.maxDelverCount = Math.floor(module.exports.MAX_SELECT_OPTIONS / 3);
