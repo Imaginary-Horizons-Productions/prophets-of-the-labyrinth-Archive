d62c8a1f2aa1b606ba43d250a03653001a6fed78
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-02-19 19:14:24 -0800

    Fixes #497

--- Source/Buttons/readymove.js
+++ Source/Buttons/readymove.js
@@ -1,7 +1,7 @@
 const Button = require('../../Classes/Button.js');
 const { MessageEmbed, MessageActionRow, MessageSelectMenu, MessageButton } = require('discord.js');
 const Delver = require('../../Classes/Delver.js');
-const { SAFE_DELIMITER, MAX_MESSAGE_ACTION_ROWS } = require('../../constants.js');
+const { SAFE_DELIMITER } = require('../../constants.js');
 const { getEmoji, getWeakness, getColor } = require('../elementHelpers.js');
 const { getAdventure } = require('../adventureDAO.js');
 const { getFullName } = require("../combatantDAO.js");
