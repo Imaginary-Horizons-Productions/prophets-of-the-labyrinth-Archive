b301bbb17e54f2992178f2951a10fbbdfca14c98
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-03-19 12:44:22 -0700

    update addResource to reduce duplicated interfaces

--- Source/adventureDAO.js
+++ Source/adventureDAO.js
@@ -23,6 +23,7 @@ const { setPlayer, getPlayer } = require("./playerDAO.js");
 const { renderRoom, updateRoomHeader } = require("./roomDAO.js");
 const { getEquipmentProperty } = require("./equipment/_equipmentDictionary.js");
 const { ThreadChannel } = require("discord.js");
+const Resource = require("../Classes/Resource.js");
 
 const adventureDictionary = new Map();
 
@@ -164,15 +165,15 @@ exports.nextRoom = function (roomType, thread) {
 		switch (resource) {
 			case "challenge":
 				rollChallenges(count, adventure).forEach(challengeName => {
-					adventure.addResource(challengeName, resource, true, visibility, 0);
+					adventure.addResource(new Resource(challengeName, resource, true, visibility, 0, uiGroup));
 				})
 				break;
 			case "equipment":
 				let tier = unparsedTier;
 				for (let i = 0; i < count; i++) {
 					if (unparsedTier === "?") {
-						let threshold = 1 + cloverCount;
-						let max = 8 + cloverCount;
+						const threshold = 1 + cloverCount;
+						const max = 8 + cloverCount;
 						adventure.updateArtifactStat("Negative-One Leaf Clover", "Expected Extra Rare Equipment", (threshold / max) - (1 / 8));
 						if (generateRandomNumber(adventure, max, "general") < threshold) {
 							tier = "Rare";
@@ -181,26 +182,26 @@ exports.nextRoom = function (roomType, thread) {
 						}
 					}
 					const equipName = rollEquipmentDrop(tier, adventure);
-					adventure.addResource(equipName, resource, 1, visibility, parseCount(unparsedCost, getEquipmentProperty(equipName, "cost", resource)), uiGroup);
+					adventure.addResource(new Resource(equipName, resource, 1, visibility, parseCount(unparsedCost, getEquipmentProperty(equipName, "cost", resource)), uiGroup));
 				}
 				break;
 			case "scouting":
-				adventure.addResource("bossScouting", resource, true, visibility, adventure.calculateScoutingCost("Final Battle"), uiGroup);
-				adventure.addResource("guardScouting", resource, true, visibility, adventure.calculateScoutingCost("Artifact Guardian"), uiGroup);
+				adventure.addResource(new Resource("bossScouting", resource, true, visibility, adventure.calculateScoutingCost("Final Battle"), uiGroup));
+				adventure.addResource(new Resource("guardScouting", resource, true, visibility, adventure.calculateScoutingCost("Artifact Guardian"), uiGroup));
 				break;
 			case "artifact":
 				const artifact = rollArtifact(adventure);
-				adventure.addResource(artifact, resource, count, visibility, "0", uiGroup);
+				adventure.addResource(new Resource(artifact, resource, count, visibility, "0", uiGroup));
 				break;
 			case "consumable":
 				const consumable = rollConsumable(adventure);
-				adventure.addResource(consumable, resource, count, visibility, "0", uiGroup);
+				adventure.addResource(new Resource(consumable, resource, count, visibility, "0", uiGroup));
 				break;
 			case "gold":
 				// Randomize loot gold
-				adventure.addResource(resource, resource, visibility === "loot" ? Math.ceil(count * (90 + generateRandomNumber(adventure, 21, "general")) / 100) : count, visibility, "0", uiGroup);
+				adventure.addResource(new Resource(resource, resource, visibility === "loot" ? Math.ceil(count * (90 + generateRandomNumber(adventure, 21, "general")) / 100) : count, visibility, "0", uiGroup));
 			default:
-				adventure.addResource(resource, resource, count, visibility, "0", uiGroup);
+				adventure.addResource(new Resource(resource, resource, count, visibility, "0", uiGroup));
 		}
 	}
 
@@ -398,17 +399,18 @@ exports.endRound = async function (adventure, thread) {
 			} else {
 				// Equipment drops
 				const dropThreshold = 1;
-				const dropMax = 8;
+				const dropMax = 16;
 				if (generateRandomNumber(adventure, dropMax, "general") < dropThreshold) {
 					const cloverCount = adventure.getArtifactCount("Negative-One Leaf Clover");
 					let tier = "Common";
 					const upgradeThreshold = 1 + cloverCount;
 					const upgradeMax = 8 + cloverCount;
+					adventure.updateArtifactStat("Negative-One Leaf Clover", "Expected Extra Rare Equipment", (upgradeThreshold / upgradeMax) - (1 / 8));
 					if (generateRandomNumber(adventure, upgradeMax, "general") < upgradeThreshold) {
 						tier = "Rare";
 					}
 					const droppedEquip = rollEquipmentDrop(tier, adventure);
-					adventure.addResource(droppedEquip, "equipment", 1, "loot", 0);
+					adventure.addResource(new Resource(droppedEquip, "equipment", 1, "loot", 0));
 				}
 				return thread.send(renderRoom(adventure, thread, lastRoundText));
 			}
