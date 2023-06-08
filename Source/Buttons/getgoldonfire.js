1ab5e96ca4cdd2cb6189ea3ae93b75e288f5d49c
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-06-08 10:59:20 -0700

    button and select refactors - combined replaceequipment and replaceequipmenttreasure - removed vestigial checks for interaction by non-party member - use payHP in getgoldonfire and hpshare

--- Source/Buttons/getgoldonfire.js
+++ Source/Buttons/getgoldonfire.js
@@ -1,35 +1,29 @@
 const Button = require('../../Classes/Button.js');
 const { generateRandomNumber } = require('../../helpers.js');
 const { getAdventure, completeAdventure, setAdventure } = require('../adventureDAO.js');
-const { dealDamage } = require("../combatantDAO.js");
+const { payHP } = require("../combatantDAO.js");
 const { editButtons } = require('../roomDAO.js');
 
 const id = "getgoldonfire";
 module.exports = new Button(id, (interaction, args) => {
 	// Gold +50, HP -100
-	let adventure = getAdventure(interaction.channel.id);
-	let delver = adventure.delvers.find(delver => delver.id == interaction.user.id);
-	let goldCount = adventure.room.resources.gold.count;
+	const adventure = getAdventure(interaction.channel.id);
+	const delver = adventure.delvers.find(delver => delver.id == interaction.user.id);
+	const goldCount = adventure.room.resources.gold.count;
 	const damageChance = 50;
-	if (delver) {
-		adventure.gainGold(goldCount);
-		if (generateRandomNumber(adventure, 101, "general") > damageChance) { // deal damage on chance 50%
-			dealDamage(delver, null, 100, true, "Untyped", adventure).then(damageText => {
-				const resultText = `${interaction.user} reaches in to grab some coin. Oh no! The gold burst into flames ${damageText}`;
-				if (adventure.lives < 1) {
-					interaction.update({ components: [] });
-					interaction.channel.send(completeAdventure(adventure, interaction.channel, "defeat", resultText));
-				} else {
-					interaction.update({ components: editButtons(interaction.message.components, { [id]: { preventUse: true, label: `+${goldCount} gold`, emoji: "✔️" } }) })
-					interaction.channel.send(resultText);
-					setAdventure(adventure);
-				}
-			});
-		} else { // just take the gold
-			interaction.update({ components: editButtons(interaction.message.components, { "getgoldonfire": { preventUse: true, label: `+${goldCount} gold`, emoji: "✔️" } }) })
+	adventure.gainGold(goldCount);
+	if (generateRandomNumber(adventure, 101, "general") > damageChance) { // deal damage on chance 50%
+		const resultText = `${interaction.user} reaches in to grab some coin. Oh no! The gold burst into flames ${payHP(delver, 100, adventure)}`;
+		if (adventure.lives < 1) {
+			interaction.update({ components: [] });
+			interaction.channel.send(completeAdventure(adventure, interaction.channel, "defeat", resultText));
+		} else {
+			interaction.update({ components: editButtons(interaction.message.components, { [id]: { preventUse: true, label: `+${goldCount} gold`, emoji: "✔️" } }) })
+			interaction.channel.send(resultText);
 			setAdventure(adventure);
 		}
-	} else {
-		interaction.reply({ content: "Please burn yourself on gold in adventures you've joined.", ephemeral: true });
+	} else { // just take the gold
+		interaction.update({ components: editButtons(interaction.message.components, { "getgoldonfire": { preventUse: true, label: `+${goldCount} gold`, emoji: "✔️" } }) })
+		setAdventure(adventure);
 	}
 });
