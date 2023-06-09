a74902fd6e24acb062abfb3b2ccfb0c595e8528a
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-06-09 10:23:13 -0700

    shortened Rushing description

--- Source/Challenges/rushing.js
+++ Source/Challenges/rushing.js
@@ -1,6 +1,6 @@
 const ChallengeTemplate = require("../../Classes/ChallengeTemplate.js");
 
-module.exports = new ChallengeTemplate("Rushing", "When choosing future rooms there's a @{intensity}% chance room type will be unknown.")
+module.exports = new ChallengeTemplate("Rushing", "There's a @{intensity}% chance room type will be unknown.")
 	.setIntensity(25)
 	.setDuration(null)
 	.setScoreMultiplier(1.5)
