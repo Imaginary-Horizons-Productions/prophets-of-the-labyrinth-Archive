2a13b162e82de5b08c7ab57bc0a9a573aec6f353
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-07-14 11:26:00 -0700

    element swap button validates a change will happen

--- Source/Buttons/elementswap.js
+++ Source/Buttons/elementswap.js
@@ -6,6 +6,11 @@ module.exports = new Button(customId, (interaction, args) => {
 	// Switch the adventurer's element to the given element
 	const adventure = getAdventure(interaction.channel.id);
 	const delver = adventure.delvers.find(delver => delver.id == interaction.user.id);
+	if (delver.element == adventure.room.element) {
+		interaction.reply({ content: `You are already ${adventure.room.element}.`, ephemeral: true });
+		return;
+	}
+
 	adventure.gainGold(200);
 	delver.element = adventure.room.element;
 	interaction.reply(`${interaction.user} signs the contract and becomes ${adventure.room.element} element.`).then(() => {
