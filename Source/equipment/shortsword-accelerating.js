4ca0a5c44c8ad278bb95744b7822d8f0356f9459
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-09-04 16:16:37 -0700

    fix all shortswords being toxic

--- Source/equipment/shortsword-accelerating.js
+++ Source/equipment/shortsword-accelerating.js
@@ -1,7 +1,7 @@
 const EquipmentTemplate = require('../../Classes/EquipmentTemplate.js');
 const { dealDamage, addModifier } = require("../combatantDAO.js");
 
-module.exports = new EquipmentTemplate("Shortsword", "Strike a foe for @{damage} @{element} damage, then apply @{mod1Stacks} @{mod1} to the foe and @{mod1Stacks} @{mod1} and @{mod2Stacks} @{mod2} to yourself", "Damage x@{critBonus}", "Fire", effect, ["Toxic Shortsword"])
+module.exports = new EquipmentTemplate("Accelerating Shortsword", "Strike a foe for @{damage} @{element} damage, then apply @{mod1Stacks} @{mod1} to the foe and @{mod1Stacks} @{mod1} and @{mod2Stacks} @{mod2} to yourself", "Damage x@{critBonus}", "Fire", effect, ["Toxic Shortsword"])
 	.setCategory("Weapon")
 	.setTargetingTags({ target: "single", team: "enemy" })
 	.setModifiers([{ name: "Stagger", stacks: 1 }, { name: "Exposed", stacks: 1 }, { name: "Quicken", stacks: 1 }])
