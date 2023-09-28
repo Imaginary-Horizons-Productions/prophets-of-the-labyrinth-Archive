dc36a9571c74d6258c7997c4b0a2422163eee1a4
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-09-27 22:00:48 -0700

    fix typo in amethyst spyglass scaling description

--- Source/Artifacts/amethystspyglass.js
+++ Source/Artifacts/amethystspyglass.js
@@ -1,5 +1,5 @@
 const Artifact = require("../../Classes/Artifact.js");
 
-module.exports = new Artifact("Amethyst Spyglass", "Get a discount on scouting of @{copies*15} gold.", "Increase discount by 5g per spyglass")
+module.exports = new Artifact("Amethyst Spyglass", "Get a discount on scouting of @{copies*15} gold.", "Increase discount by 15g per spyglass")
 	.setElement("Untyped")
 	.setFlavorText({ name: "*Additional Notes*", value: "*Peering through it makes things look blocky*" })
